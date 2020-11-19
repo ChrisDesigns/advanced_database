3f98ed2dab992df3ec0146ea9bd8f108870b615a
Dr. Gregory DeLozier <gdelozie@kent.edu>
2020-11-19 01:37:42 +0000

    pico stuff

--- pico.py
+++ pico.py
@@ -11,6 +11,9 @@ def everything(v):
 def always(v):
     return True
 
+def never(v):
+    return False
+
 def open_database(name):
     global filename
     global items
@@ -31,20 +34,42 @@ def insert(data):
     items.append(data)
     commit()
 
-def query(select, where):
+def query(select=everything, where=always):
     return [select(item) for item in items if where(item)]
+
+def delete(where=never):
+    global items
+    kept_items = [item for item in items if not where(item)]
+    items = kept_items
+    commit()
+
+def update(where=never, content=lambda v:{}):
+    global items
+    for item in items:
+        if where(item):
+            new_values = content(item)
+            for key in new_values.keys():
+                item[key] = new_values[key]
+    commit()
 
 if __name__ == "__main__":
     open_database("pico.pkl")
-    # insert({"id":1, "task":"do something useful", "status":0})
-    # insert({"id":2, "task":"do something else useful", "status":0})
-    #insert({"id":3, "task":"do something enjoyable", "status":0})
+    delete(where=always)
+    insert({"id":1, "task":"do something useful", "status":0})
+    insert({"id":2, "task":"do something else useful", "status":0})
+    insert({"id":3, "task":"do something enjoyable", "status":0})
     select = lambda v : v
     where = lambda v : True
-    for item in query(select=everything, where=always):
-        print(item)
-    for item in query(select=lambda v:v['task'], where=lambda v:v['id']==3):
-        print(item)
+    # for item in query(select=everything, where=always):
+    #     print(item)
+    # for item in query(select=lambda v:v['task'], where=lambda v:v['id']==3):
+    #     print(item)
+    # print(query(select=everything, where=lambda v:v['id']==1))
+    # delete(where=lambda v:v['id']==1)
+    # print(query(select=everything, where=lambda v:v['id']==1))
+    print(query(select=everything, where=lambda v:v['id']==2))
+    update(where=lambda v:v['id']==2, content=lambda v:{'status':v['status']+1})
+    print(query(select=everything, where=lambda v:v['id']==2))
 
 
 
